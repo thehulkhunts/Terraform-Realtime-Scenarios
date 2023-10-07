46cdca0b38d95329c511225e9c6fc55cfc7e434b
Vinay Jadhav <43322698+thehulkhunts@users.noreply.github.com>
2023-10-07 19:00:00 +0530

    Update variable.tf
    update variable list

--- project-eks/eks-cluster/variable.tf
+++ project-eks/eks-cluster/variable.tf
@@ -1,6 +1,8 @@
 variable "eks-iam-role" {}
 variable "private-subnet-01" {}
 variable "private-subnet-02" {}
+variable "subnet-01" {}
+variable "subnet-02" {}
 variable "aws-iam-policy-attachment" {}
 variable "cluster-name" {}
 variable "node-role" {}
