7c4da561e4b7740ab41b9a5b0086b083a3bc3c81
Vinay Jadhav <43322698+thehulkhunts@users.noreply.github.com>
2023-10-07 19:14:57 +0530

    Update main.tf
    update module main with: AWS provider version and Terraform updated version in module-main

--- project-eks/module-main/main.tf
+++ project-eks/module-main/main.tf
@@ -4,11 +4,11 @@ provider "aws" {
 }
 
 terraform {
-  required_version = "1.5.6"
+  required_version = "~>= 1.5.6"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.65.0"
+      version = "~>= 5.19.0"
     }
   }
 }
